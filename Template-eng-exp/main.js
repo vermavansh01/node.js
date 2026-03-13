599e1be0087aebbeecd9a2ae25fe22cf10bb75a8
Prince Soni <120249334+Princesoni90@users.noreply.github.com>
2026-03-13 17:51:51 +0530

    change

--- Template-eng-exp/main.js
+++ Template-eng-exp/main.js
@@ -1,11 +1,27 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  let siteName = "Addidas";
+  let searchText = "Search Now";
+  res.render("index", {
+    siteName,
+    searchText,
+  });
+});
+
+app.get("/blog/:slug", (req, res) => {
+  let blogTitle = "addidas why and when?";
+  let blogContent = "it's a very good brand"; 
+  res.render("blogpost", {
+    blogTitle: blogTitle,
+    blogContent: blogContent,
+  });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
