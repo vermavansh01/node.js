df1c75fd490619dc9ce7255a3dcb4f0e2f3afcf5
Prince Soni <120249334+Princesoni90@users.noreply.github.com>
2026-03-14 10:50:58 +0530

    c

--- Template-eng-exp/main.js
+++ Template-eng-exp/main.js
@@ -4,12 +4,14 @@ const port = 3000;
 
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
+app.get("/", (req, res) => { 
   let siteName = "Addidas";
   let searchText = "Search Now";
+  let arr =[1,54,60];
   res.render("index", {
     siteName,
     searchText,
+    arr
   });
 });
 
@@ -17,11 +19,11 @@ app.get("/blog/:slug", (req, res) => {
   let blogTitle = "addidas why and when?";
   let blogContent = "it's a very good brand"; 
   res.render("blogpost", {
-    blogTitle: blogTitle,
-    blogContent: blogContent,
+    blogTitle,
+    blogContent,
   });
-});
+}); 
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
-});
+}); 
